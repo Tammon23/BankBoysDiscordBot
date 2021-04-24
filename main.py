003bdc12f0b7f129c3b0fa4c5258e66d26f23293
Tammon23 <Tammon2000@gmail.com>
2021-04-24 15:27:36 -0400

    Added ping command that shows the bot's latency

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import requests
 import discord
+from discord.ext import commands
 import os
-#🌟 remember pip3 install python-dotenv
+# 🌟 remember pip3 install python-dotenv
 from dotenv import load_dotenv
 
+bot = commands.Bot(command_prefix='!')
 
 client = discord.Client()
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
+
 @client.event
 async def on_ready():
     print(f'This: {client.user} has connected')
 
-client.run(TOKEN)
+
+# client.run(TOKEN)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Bot latency is: {round(bot.latency * 1000)}ms")
+
+
+bot.run(TOKEN)
 
 if __name__ == "__main__":
     data = requests.get("https://api.binance.com/api/v3/ticker/price", params={"symbol": "BTCUSDT"}).json()
