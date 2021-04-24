684a3152374e42c5febe5c728d4652e2b38afe7b
Bill Shema <shema.bill2@gmail.com>
2021-04-24 14:48:32 -0400

    Working bot test

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
 import discord
+import os
+#🌟 remember pip3 install python-dotenv
+from dotenv import load_dotenv
+
+
+client = discord.Client()
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+@client.event
+async def on_ready():
+    print(f'This: {client.user} has connected')
+
+client.run(TOKEN)
 
 if __name__ == "__main__":
     data = requests.get("https://api.binance.com/api/v3/ticker/price", params={"symbol": "BTCUSDT"}).json()
