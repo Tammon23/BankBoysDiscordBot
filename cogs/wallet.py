c83e59be0e216642e67cbc47c01663caa0cce0aa
Tammon23 <Tammon2000@gmail.com>
2021-04-28 02:29:04 -0400

    Added wallet cog
    - Currently houses the deposit function which is used to track how much of a coin was bought by a user
    
    * wallets are currently different for each guild

---
+++ cogs/wallet.py
@@ -0,0 +1,46 @@
+import time
+import logging
+from discord.ext import commands
+from database import Database as db
+
+
+class Wallet(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.db = db()
+        self.db.connect_to_db()
+
+    @staticmethod
+    def db_deposit(conn, guild, user, coinsymbol, price, savedate):
+        with conn.cursor() as cur:
+            # inserting into the db the new data
+            cur.execute("INSERT INTO wallet (guildid, userid, coinsymbol, price, savedate) VALUES (%s, %s, %s, %s, %s);"
+                        , (guild, user, coinsymbol, price, savedate))
+
+        conn.commit()
+        logging.debug("deposit(): status message: %s", cur.statusmessage)
+
+    @commands.command(help="!deposit coin, price")
+    async def deposit(self, ctx, *args):
+        if len(args) != 2:
+            await ctx.send("Incorrect usage.")
+
+        else:
+            coin, price = args
+            price = str(price)
+            price.replace("$", "")
+
+            guild = str(ctx.guild.id)
+            author = str(ctx.author.id)
+
+            try:
+                self.db.run_transaction(lambda conn: self.db_deposit(conn, guild, author, coin, price, time.time_ns()))
+                await ctx.send(f"Updating wallet successful!")
+
+            except ValueError as ve:
+                logging.debug(f"deposit(ctx, *args) failed {ve}")
+                await ctx.send(f"Updating wallet failed!")
+
+
+def setup(client):
+    client.add_cog(Wallet(client))
