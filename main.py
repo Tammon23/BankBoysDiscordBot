53aa9249c22eef1498d886eb289f8e0fbb1cdf45
Tammon23 <Tammon2000@gmail.com>
2021-04-28 02:31:52 -0400

    Added a feature that iteratively loads all cogs in a directory

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import requests
-import discord
 from discord.ext import commands
 import os
 # 🌟 remember pip3 install python-dotenv
 from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix='!', help_command= None)
+bot = commands.Bot(command_prefix='!', help_command=None)
+
 
 @bot.event
 async def on_ready():
     print(f'This: {bot.user} has connected')
 
 
-#ctx is short for context 
-#joshy commands 🚀 
+# ctx is short for context
+# joshy commands 🚀
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"Bot latency is: {round(bot.latency * 1000)}ms")
+
 
 @bot.command()
 async def help(ctx):
@@ -26,7 +27,7 @@ async def help(ctx):
 async def coin(ctx, args):
     ref = args.upper()
     print(f'Symbol: {ref}')
-    values = requests.get("https://api.binance.com/api/v3/ticker/price", params={"symbol": f'{ref}'}).json()
+    values = requests.get("https://api.binance.com/api/v3/ticker/price", params={"symbol": ref}).json()
     for key, value in values.items():
         print(f'{key} : {value}')
         print(value)
@@ -34,7 +35,11 @@ async def coin(ctx, args):
 
 
 if __name__ == "__main__":
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+
     load_dotenv()
-    #token for joshy jr
+    # token for joshy jr
     TOKEN = os.getenv('TOKEN')
     bot.run(TOKEN)
